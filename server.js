30ccff943768c95eaa5c42bacab740426ed4cd16
‘Tim <timothy.j.ocampo@gmail.com>
2019-08-19 16:11:58 -0700

    removed if statement from server.js : already in production mode.

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('dist'));
-    app.get('*', (req, res)=>{
-        res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
-    })
-}
+// if(process.env.NODE_ENV === 'production'){
+app.use(express.static('dist'));
+app.get('*', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
+});
+// }
 
 
 const port = process.env.PORT || 5000;
