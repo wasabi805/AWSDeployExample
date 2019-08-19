e9483ece8a50d49b57581c202827c71fb505018e
‘Tim <timothy.j.ocampo@gmail.com>
2019-08-19 13:00:49 -0700

    added Content-Security-Policy to server . see ln 7 of server.js

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const app = express();
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('dist'));
     app.get('*', (req, res)=>{
+        res.setHeader('Content-Security-Policy', "script-src 'self'");
         res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
     })
 }
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, ()=>{
